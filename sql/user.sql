c22052565dd0d3da6632fb49e70e2f3a15e191bd
isac322 <bh322yoo@gmail.com>
2017-05-16 13:19:04 +0900

    add is_dropped field on user table

--- sql/user.sql
+++ sql/user.sql
@@ -1,8 +1,9 @@
 CREATE TABLE user (
-    student_id VARCHAR(32)             NOT NULL PRIMARY KEY,
-    name       VARCHAR(100)
-               CHARACTER SET utf8
-               COLLATE utf8_unicode_ci NOT NULL,
-    is_admin   BOOLEAN                 NOT NULL DEFAULT 0,
-    created    TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP
+	student_id VARCHAR(32)             NOT NULL PRIMARY KEY,
+	name       VARCHAR(100)
+			   CHARACTER SET utf8
+			   COLLATE utf8_unicode_ci NOT NULL,
+	is_admin   BOOLEAN                 NOT NULL DEFAULT 0,
+	is_dropped BOOLEAN                 NOT NULL DEFAULT FALSE,
+	created    TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
